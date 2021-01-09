b116a7e8c56b7ff3466919a556576f235b11ff14
Ivan <nasekinid8591@yandex.ru>
2021-01-10 02:08:59 +0300

    refuctoring random planet, add loader

---
+++ src/components/loader/loader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loadingio-spinner-spinner-94ic29e0ryr">
+            <div className="ldio-nvphrpvwc7c">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
