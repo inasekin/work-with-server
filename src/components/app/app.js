96173aee73cb3381fc57d70c21079a7a4862cc85
Ivan <nasekinid8591@yandex.ru>
2021-01-13 01:20:26 +0300

    studying debug, fix errors

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,24 +2,16 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 
 import './app.css';
 import ErrorIndicator from '../error-indicator/error-indicator';
+import PeoplePage from '../people-page/people-page';
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
     hasError: false
-  };
-
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    })
   };
 
   componentDidCatch() {
@@ -39,14 +31,9 @@ export default class App extends Component {
       <RandomPlanet />
 
       <div className="container">
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+      <PeoplePage/>
+      <PeoplePage/>
+      <PeoplePage/>
       </div>
     </div>
     );
