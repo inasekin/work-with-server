5a5520ed17ddb432a3545962bf0a8ff8c52d893b
Ivan <nasekinid8591@yandex.ru>
2021-01-14 23:37:29 +0300

    use function, refuctoring rendering

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import ItemList from '../item-list/';
+import PersonDetails from '../person-details/person-details'
 
 import './app.css';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import PeoplePage from '../people-page/people-page';
+import SwapiService from '../../api/swapi-service';
 
 export default class App extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true,
@@ -32,8 +37,24 @@ export default class App extends Component {
 
       <div className="container">
       <PeoplePage/>
-      <PeoplePage/>
-      <PeoplePage/>
+      <div className="row mb2 margin-bottom">
+          <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} 
+                    getData={this.swapiService.getAllPlanets}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+      </div>
+      <div className="row mb2 margin-bottom">
+          <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} 
+                    getData={this.swapiService.getAllStarships}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+      </div>
       </div>
     </div>
     );
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -4,8 +4,11 @@ import './people-page.css';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
+import SwapiService from '../../api/swapi-service';
 
 export default class PeoplePage extends Component {
+
+    swapiService = new SwapiService();
 
     state = {
         selectedPerson: 3,
@@ -33,7 +36,8 @@ export default class PeoplePage extends Component {
         return (
             <div className="row mb2 margin-bottom">
                 <div className="col-md-6">
-                <ItemList onItemSelected={this.onPersonSelected} />
+                <ItemList onItemSelected={this.onPersonSelected}
+                            getData={this.swapiService.getAllPeople} />
                 </div>
                 <div className="col-md-6">
                 <PersonDetails personId={this.state.selectedPerson} />
