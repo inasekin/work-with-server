752cc563c852bfbb0f2d17c80502274d02e3d302
Ivan <nasekinid8591@yandex.ru>
2021-01-29 15:15:20 +0300

    finish part of refuctoring, change render starshiplist

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -23,10 +23,11 @@ const withChildFunction = (Wrapped, fn) => {
 // );
 
 const renderName = ({ name }) => <span>{name}</span>;
+const renderModelAndName = ({ model, name }) => <span>{name} ({model})</span>
 
 const PersonList = withData(withChildFunction(ItemList, renderName), getAllPeople);
 const PlanetList = withData(withChildFunction(ItemList, renderName), getAllPlanets);
-const StarshipList = withData(withChildFunction(ItemList, renderName), getAllStarships);
+const StarshipList = withData(withChildFunction(ItemList, renderModelAndName), getAllStarships);
 
 export {
     PersonList,
